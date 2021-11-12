c72b0128caef7d0cc6e46ddb1ba6cc88881ea10a
yoon92kr <yoon92_kr@naver.com>
2021-11-12 23:02:19 +0900

    2021.11.12 표현식 ▶ JavaScript에서 문자열 내 표현식을 통해 변수를 호출할 수 있다. 단 작은따옴표(''), 큰따옴표("")가 아닌 백틱(``)으로만 가능하다.

---
+++ Code006.js
@@ -0,0 +1,9 @@
+const name = 'yoon';
+
+const message1 = `My name is ${name}`;
+const message2 = 'My name is ${name}';
+const message3 = "My name is ${name}";
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
